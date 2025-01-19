c653c6522763d56782db959edab68c2634fb193f
joeljoanlopez <joeljoanlopez@gmail.com>
2025-01-19 11:42:38 +0100

    Create pigeon factory to spawn pigeons

---
+++ main/Resources/Pigeon.go
@@ -0,0 +1,16 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"Pigeon\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/Resources/Textures.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.12
+    y: 0.12
+    z: 0.12
+  }
+}
